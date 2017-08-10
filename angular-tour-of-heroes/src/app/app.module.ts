221b29d437443caef1709d2e739bf5661c0541dd
Nicolas Constantin <nychollas88@gmail.com>
2017-08-10 18:55:00 +0300

    Added a mocked web api and suport for updating a hero.

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module'
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component'
@@ -9,19 +14,19 @@ import { HeroesComponent } from './heroes.component'
 import { HeroService } from './hero.service'
 import { DashboardComponent } from './dashboard.component'
 
-import { AppRoutingModule } from './app-routing.module'
-
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
     DashboardComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
