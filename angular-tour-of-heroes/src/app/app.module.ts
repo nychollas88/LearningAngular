5402951cec4d7e5409220b3be9708050c189b3bc
Nicolas Constantin <nychollas88@gmail.com>
2017-08-10 15:26:47 +0300

    Angular routing tutorial - Turn AppComponent into an application shell that only handles navigation - Relocate the Heroes concerns within the current AppComponent to a separate HeroesComponent - Add routing - Create a new DashboardComponent - Tie the Dashboard into the navigation structure

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,19 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component'
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component'
+import { HeroesComponent } from './heroes.component'
+import { HeroService } from './hero.service'
+import { DashboardComponent } from './dashboard.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }])
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
